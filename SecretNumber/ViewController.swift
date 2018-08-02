3c9c990f4303a003213a8288e0ef3503c086c394
Alban BERNARD <alban.bernard@gmail.com>
2018-08-02 16:54:54 +0200

    Initial Commit

---
+++ SecretNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecretNumber
+//
+//  Created by Alban BERNARD on 02/08/2018.
+//  Copyright © 2018 Alban BERNARD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
